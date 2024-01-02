b17c3443e187aa36f61c7dce0c2a690b875a8772
scherepanov <sergey@cherepanov.org.ua>
2024-01-03 02:00:58 +0400

    modified:   Formula/devserver-sh.rb

--- Formula/devserver-sh.rb
+++ Formula/devserver-sh.rb
@@ -3,11 +3,13 @@ class DevserverSh < Formula
   
     desc "Package acting as bridge between Node projects and their package managers"
     homepage "https://github.com/digitalspacestdio/cli.devserver.sh"
-    url "https://github.com/digitalspacestdio/cli.devserver.sh/releases/download/0.0.2/0.0.2.zip"
-    sha256 "ff05fef766d466f34e1f8af59ded7ced8b7d26d48649d73aa1178f941291abbe"
+    url "https://github.com/digitalspacestdio/cli.devserver.sh/releases/download/0.0.3/0.0.3.zip"
+    sha256 "d4394e66c42cda1d5fbe11fd92243816fbc5067ac601f67abe6b4a4de4bfeba9"
     license "MIT"
+    revision 1
   
-    depends_on "node"
+    depends_on "node@18"
+    depends_on "nest"
   
     def install
       system "npm", "install", *Language::Node.std_npm_install_args(libexec)
