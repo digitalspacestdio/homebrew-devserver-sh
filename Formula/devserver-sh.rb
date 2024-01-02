eb32a9a516a3ce7c131f7b5e53515f79149e1bd0
scherepanov <sergey@cherepanov.org.ua>
2024-01-03 02:11:13 +0400

    modified:   Formula/devserver-sh.rb

--- Formula/devserver-sh.rb
+++ Formula/devserver-sh.rb
@@ -9,10 +9,10 @@ class DevserverSh < Formula
     revision 1
   
     depends_on "node@18"
-    depends_on "nest"
   
     def install
-      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-      bin.install_symlink Dir["#{libexec}/bin/*"]
+        ENV["PATH"] = "#{buildpath}/node_modules/.bin:" + ENV["PATH"]
+        system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+        bin.install_symlink Dir["#{libexec}/bin/*"]
     end
   end
